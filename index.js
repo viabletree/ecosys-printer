aa64d7ac42c3596695133efbac635f0b2585fd85
Zafar Abbas <zafar.abbas@viabletree.com>
2026-06-09 12:19:45 +0500

    fix: increase payload size limit for barcode generation requests

--- index.js
+++ index.js
@@ -31,7 +31,9 @@ const {
 const uploadDir = process.env.UPLOAD_DIR;
 
 const app = express();
-app.use(express.json());
+
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json({ limit: "50mb" }));
 app.use(cors());
 
 app.post("/api/generate-barcodes", async (req, res) => {
